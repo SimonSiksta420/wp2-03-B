ec14382c541b64f1b9f289730a78e8f668ee676a
SimonSiksta420 <simon.siksta@student.ossp.cz>
2020-09-29 18:42:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,44 @@
-<?php
-define('EUR_CZK_RATE', 27);
-define('USD_CZK_RATE', 23);
-define('GPK_CZK_Rate', 30);
-?>
-
+<?php 
+ $submit = filter_input(INPUT_POST, 'submit');
+ $amount = filter_input(INPUT_POST, 'amount');
+ $switch = filter_input(INPUT_POST, 'converter');
+ define('EUR_CZK', 27);
+ define('USD_CZK', 23);
+ define('GBP_CZK', 30);
+ ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="cz">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title> Penežní converter </title>
 </head>
 
 <body>
 
+<?php
 
+if(isset($submit)) { ?>
+
+ <p> <?= $money ?> </p>
+
+<?php } else { ?>
 
 <form action="index.php" method="post">
 
- <p> Kolik penež <input type="text" name="penize"> </p>
+<p> Kolik penež <input type="text" name="penize"> </p>
  <p> Czk do Eur <input type="radio" value="czkdoeuro" name="converter"> </p>
  <p> Euro do czk <input type="radio" value="eurodoczk" name="converter"> </p>
+ <p> Czk do USD <input type="radio" value="czkdousd" name="converter"> </p>
+ <p> USD do Czk  <input type="radio" value="usddoczk" name="converter"> </p>
+ <p> GP do Czk <input type="radio" value="czkdousd" name="converter"> </p>
+ <p> Czk do GP  <input type="radio" value="usddoczk" name="converter"> </p>
+
 
  <input type="submit" name="submit" value="Odeslat">
 
 </form>
-
+<?php  }  ?>
+</form>
 </body>
-
 </html>
